51d02da5a4d1424bdec7ad22983973f04ef20687
Jishnu Nair <nair.jishnu2016@gmail.com>
2022-04-03 05:22:16 +0000

    Updated top_directors

--- 7_project/dbt/models/core/top_directors.sql
+++ 7_project/dbt/models/core/top_directors.sql
@@ -7,13 +7,11 @@ name_id
 from {{ ref('stg_director_writer') }} dir,
 unnest(dir.directors) name_id
 ) 
-select * from (
 select people.primary_name, basics.release_year, count(1) as title_count, 
-round(avg(ratings.avg_rating),2) as average_rating, floor(avg(ratings.votes_count)) as average_votes,
-rank() over (partition by people.primary_name, basics.release_year order by avg(ratings.avg_rating) desc,avg(ratings.votes_count) desc) as seq
+round(avg(ratings.avg_rating),2) as average_rating, floor(avg(ratings.votes_count)) as average_votes
 from {{ ref('stg_title_basics') }} basics
 join director_split on basics.title_id = director_split.title_id
 join {{ ref('stg_people') }} people on people.name_id = director_split.name_id
 join {{ ref('stg_title_ratings') }} ratings on basics.title_id = ratings.title_id
 group by people.primary_name, basics.release_year
-) A where A.seq <= 20
+order by average_rating desc, average_votes desc
