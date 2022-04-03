219acd18c950e84873b49506e228e70b1dfdad90
Jishnu Nair <nair.jishnu2016@gmail.com>
2022-04-03 03:01:56 +0000

    Updated order logic

--- 7_project/dbt/models/core/top_movies.sql
+++ 7_project/dbt/models/core/top_movies.sql
@@ -2,16 +2,10 @@
 
 select * from (
 select basics.title_id, basics.primary_title, basics.release_year,
-ARRAY(
-    select people.primary_name
-    from unnest(dir.directors) as director_id
-    join {{ ref('stg_people') }} people on director_id = people.name_id
-) as movie_directors,
 ratings.avg_rating, ratings.votes_count,
-rank() over (partition by basics.release_year order by ratings.avg_rating, ratings.votes_count desc) as rating_seq
+rank() over (partition by basics.release_year order by ratings.avg_rating desc, ratings.votes_count desc) as rating_seq
 from {{ ref('stg_title_basics') }} basics 
-join {{ ref('stg_director_writer') }} dir on dir.title_id = basics.title_id
 join {{ ref('stg_title_ratings') }} ratings on ratings.title_id = basics.title_id
-where basics.title_type in ('movie', 'short')
+where basics.title_type in ('movie')
 ) A 
 where A.rating_seq <= 20
